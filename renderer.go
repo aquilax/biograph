2f51d9749e9ad4d3796085a18133e9d68950ec9e
aquilax <aquilax@gmail.com>
2018-07-01 18:03:39 +0200

    Added renderer

---
+++ renderer.go
@@ -0,0 +1,29 @@
+package biograph
+
+import (
+	"fmt"
+	"io"
+)
+
+const dateFormat = "2006-01-02"
+
+type RenderOptions map[string]string
+
+type Renderer interface {
+	render(*Life) error
+}
+
+type TextRenderer struct {
+	out io.WriteCloser
+}
+
+func NewTextRenderer(out io.WriteCloser) *TextRenderer {
+	return &TextRenderer{out}
+}
+
+func (tr *TextRenderer) Render(life *Life) error {
+	for _, event := range life.Items() {
+		fmt.Fprintf(tr.out, "%s - %s  %s\n", event.getFrom().Format(dateFormat), event.getTo().Format(dateFormat), event.getName())
+	}
+	return nil
+}
