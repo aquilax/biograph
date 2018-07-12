664b15a40f920d356b3436af60427f1b149d4c6f
aquilax <aquilax@gmail.com>
2018-07-12 18:54:53 +0200

    Composition

--- event.go
+++ event.go
@@ -43,76 +43,69 @@ func NewHome(address, country string, from, to time.Time, meta *MetaData) *HomeE
 		meta = &MetaData{}
 	}
 	meta.merge(&MetaData{"address": address, "country": country})
-	return &HomeEvent{from, to, meta}
+	return &HomeEvent{&GenericEvent{from, to, meta}}
 }
 
 func (e *HomeEvent) GetType() EventType { return Home }
-func (e *HomeEvent) GetFrom() time.Time { return e.from }
-func (e *HomeEvent) GetTo() time.Time   { return e.to }
 func (e *HomeEvent) GetName() string    { return e.meta.Get("address") }
-func (e *HomeEvent) GetMeta() *MetaData { return e.meta }
 
 // EducationEvent represents studying in educational institution
-type EducationEvent GenericEvent
+type EducationEvent struct {
+	*GenericEvent
+}
 
 func NewEducation(school, degree string, from, to time.Time, meta *MetaData) *EducationEvent {
 	if meta == nil {
 		meta = &MetaData{}
 	}
 	meta.merge(&MetaData{"school": school, "degree": degree})
-	return &EducationEvent{from, to, meta}
+	return &EducationEvent{&GenericEvent{from, to, meta}}
 }
 
 func (e *EducationEvent) GetType() EventType { return Education }
-func (e *EducationEvent) GetFrom() time.Time { return e.from }
-func (e *EducationEvent) GetTo() time.Time   { return e.to }
 func (e *EducationEvent) GetName() string    { return e.meta.Get("school") }
-func (e *EducationEvent) GetMeta() *MetaData { return e.meta }
 
-type WorkEvent GenericEvent
+type WorkEvent struct {
+	*GenericEvent
+}
 
 func NewWork(employer, position string, from, to time.Time, meta *MetaData) *WorkEvent {
 	if meta == nil {
 		meta = &MetaData{}
 	}
 	meta.merge(&MetaData{"employer": employer, "position": position})
-	return &WorkEvent{from, to, meta}
+	return &WorkEvent{&GenericEvent{from, to, meta}}
 }
 
 func (e *WorkEvent) GetType() EventType { return Work }
-func (e *WorkEvent) GetFrom() time.Time { return e.from }
-func (e *WorkEvent) GetTo() time.Time   { return e.to }
 func (e *WorkEvent) GetName() string    { return e.meta.Get("employer") }
-func (e *WorkEvent) GetMeta() *MetaData { return e.meta }
 
-type TravelEvent GenericEvent
+type TravelEvent struct {
+	*GenericEvent
+}
 
 func NewTravel(place, country string, from, to time.Time, meta *MetaData) *TravelEvent {
 	if meta == nil {
 		meta = &MetaData{}
 	}
 	meta.merge(&MetaData{"place": place, "country": country})
-	return &TravelEvent{from, to, meta}
+	return &TravelEvent{&GenericEvent{from, to, meta}}
 }
 
 func (e *TravelEvent) GetType() EventType { return Travel }
-func (e *TravelEvent) GetFrom() time.Time { return e.from }
-func (e *TravelEvent) GetTo() time.Time   { return e.to }
 func (e *TravelEvent) GetName() string    { return e.meta.Get("place") + ", " + e.meta.Get("country") }
-func (e *TravelEvent) GetMeta() *MetaData { return e.meta }
 
-type ItemEvent GenericEvent
+type ItemEvent struct {
+	*GenericEvent
+}
 
 func NewItem(category string, from, to time.Time, meta *MetaData) *ItemEvent {
 	if meta == nil {
 		meta = &MetaData{}
 	}
 	meta.merge(&MetaData{"category": category})
-	return &ItemEvent{from, to, meta}
+	return &ItemEvent{&GenericEvent{from, to, meta}}
 }
 
 func (e *ItemEvent) GetType() EventType { return Item }
-func (e *ItemEvent) GetFrom() time.Time { return e.from }
-func (e *ItemEvent) GetTo() time.Time   { return e.to }
 func (e *ItemEvent) GetName() string    { return e.meta.Get("category") }
-func (e *ItemEvent) GetMeta() *MetaData { return e.meta }
