63e333dc43112cf15ca0fe5cd82f578c93e7ae3f
aquilax <aquilax@gmail.com>
2022-06-12 10:00:30 +0200

    Fix filtering

--- filter.go
+++ filter.go
@@ -6,6 +6,12 @@ type Filterer func(LifeEvent) bool
 
 func NewBetween(from, to time.Time) Filterer {
 	return func(l LifeEvent) bool {
+		if l.GetTo().IsZero() {
+			return from.Sub(l.GetFrom()) >= 0
+		}
+		if l.GetFrom().IsZero() {
+			return l.GetTo().Sub(to) >= 0
+		}
 		return from.Sub(l.GetFrom()) >= 0 && l.GetTo().Sub(to) >= 0
 	}
 }
--- filter_test.go
+++ filter_test.go
@@ -71,6 +71,15 @@ func TestNewBetween(t *testing.T) {
 			},
 			true,
 		},
+		{
+			"If event is withing the start the interval but not finished",
+			args{
+				d("2017-01-02"),
+				d("2017-01-02"),
+				NewItem("test", d("2017-01-01"), time.Time{}, nil),
+			},
+			true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
